557f3db0a275f1d17f46852fff13f1949517a93a
wly_git <2825024660@qq.com>
2022-07-31 09:59:53 +0800

    add 单分支判断code

---
+++ Python基础/Python分支练习.py
@@ -0,0 +1,17 @@
+# _*_ coding:utf-8 _*_
+# @time 2022/7/26 22:16
+# @Author wly
+# @name Python分支练习.py
+
+score = 86
+
+if 90 <= score <= 100:
+    print("A")
+elif 80 <= score < 90:
+    print("B")
+elif 70 <= score < 80:
+    print("C")
+elif 60 <= score < 70:
+    print("D")
+elif score < 60:
+    print("E")
---
+++ Python基础/Python双分支判断.py
@@ -0,0 +1,11 @@
+# _*_ coding:utf-8 _*_
+# @time 2022/7/26 22:14
+# @Author wly
+# @name Python双分支判断.py
+
+age = 18
+
+if age >= 18:
+    print("您可以上网啦")
+else:
+    print("滚回家吃饭")
